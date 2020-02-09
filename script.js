b71b8ba376569220147c31dc015b1b03555c305b
Glitch (hello-webpage) <none>
2020-02-09 05:42:17 +0000

    👱🚖 Checkpoint ./script.js:7333574/1266 ./calculator-page.html:7333574/7 ./style2.css:7333574/8

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+class Calculator {
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText
+    this.currentOperandText = currentOperandText
+    this.clear()
+  }
+  
+  //this action will delete all the values i see in the previous/current operand and the operation
+  clear() {
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
+  }
+  
+  delete() {
+    
+  }
+  appendNumber(number) {
+    this.currentOperand = number
+  }
+  
+  chooseOperation(operation) {
+    
+  }
+  
+  calculate() {
+    
+  }
+  
+  updateDisplay() {
+    this.currentOperandText.innerText = this.currentOperand
+  }
+}
+
+//created constants for each number/operation/action/ and connected them with each of the buttons on html
 const numbButtons = document.querySelectorAll('num')
 const operationButtons = document.querySelectorAll('operations')
 const equalButton = document.querySelector('equals')
@@ -5,3 +40,12 @@ const deleteButton = document.querySelector('delete')
 const clearButton = document.querySelector('clear')
 const previousOperandText = document.querySelector('previous-operand')
 const currentOperandText = document.querySelector('current-operand')
+
+const calculator = new Calculator(previousOperandText, currentOperandText)
+
+numbButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay
+  })
+})
